c068c51a64c7b1922f4db1ca285cbd2c91ec9926
caroline <the.aspiring.generalist@gmail.com>
2021-05-12 21:47:03 -0400

    Updated script.js; each game lasts five rounds, and users can decide whether they want to play again

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ function computerPlay() {
 function game() {
     let playerScore = 0;
     let computerScore = 0;
+    let currentRound = 1;
+
+    for (let i = 0; i < 5; i++) {
 
     //Computer selection
     const computerSelection = computerPlay();
@@ -25,6 +28,9 @@ function game() {
     let playerInput = prompt("Rock, Paper, or Scissors?");
     let playerSelection = playerInput.toLowerCase();
 
+    //Show round number
+    console.log(`Round ${currentRound}`);
+
     //Play a round of the game
     function playRound(playerSelection, computerSelection) {
         let result;
@@ -32,10 +38,12 @@ function game() {
             switch(computerSelection) {
                 case "paper":
                     result = "Lose!";
+                    computerScore += 1;
                     break;
 
                 case "scissors":
                     result = "Win!";
+                    playerScore += 1;
                     break;
                 
                 default:
@@ -45,10 +53,12 @@ function game() {
             switch(computerSelection) {
                 case "rock":
                     result = "Win!";
+                    playerScore += 1;
                     break;
 
                 case "scissors":
                     result = "Lose!";
+                    computerScore += 1;
                     break;
                 
                 default:
@@ -58,27 +68,58 @@ function game() {
             switch(computerSelection) {
                 case "rock":
                     result = "Lose!";
+                    computerScore += 1;
                     break;
 
                 case "paper":
                     result = "Win!";
+                    playerScore += 1;
                     break;
                 
                 default:
                     result = "Draw!"
             }
         } else {
-            result = "Please enter rock, paper, or scissors.";
+            result = ("Please enter rock, paper, or scissors.");
         }
         return result;
     }
     
-    //Show the player and computer selections
-    console.log(`Player: ${playerSelection} Computer: ${computerSelection}`);
+    function roundResult() {
+        //Show the player and computer selections
+        console.log(`Player: ${playerSelection} Computer: ${computerSelection}`);
 
-    //Show the result
-    console.log(playRound(playerSelection, computerSelection));
+        //Show the result of this round
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Your Score: ${playerScore} Computer Score: ${computerScore}`);
+    }
+    roundResult();
+    currentRound ++;
+    }
+
+    //Final results
+    if (playerScore > computerScore) {
+        console.log("Final Results: You win!");
+    } else if (playerScore < computerScore) {
+        console.log("Final Results: Computer wins!");
+    } else {
+        console.log("Final Results: Draw!");
+    }
+
+    function playAgain() {
+        let input = prompt("Would you like to play again? Yes/No");
+        if (input.toLowerCase() === "yes" || input.toLowerCase() === "y") {
+            game();
+        } else {
+            console.log("Thanks for playing!");
+        }
+    }
+
+    playAgain();
+    
 }
+
+game();
 
 /*
 const playerSelection = "scissors";
